597eed87c6c4c95a1fa3e418701cf0403509ecc2
jimbling <jimbling05@gmail.com>
2025-11-02 11:18:02 +0700

    edit migrations

--- migrations/20251102041739-add_welcome_to_contacts.js
+++ migrations/20251102041739-add_welcome_to_contacts.js
@@ -2,21 +2,14 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('contacts', 'last_active_at', {
-      type: Sequelize.DATE,
-      allowNull: true,
-      after: 'is_subscribed',
-    });
-
     await queryInterface.addColumn('contacts', 'is_welcome_sent', {
       type: Sequelize.BOOLEAN,
       defaultValue: false,
-      after: 'last_active_at',
+      after: 'last_active_at'
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('contacts', 'last_active_at');
     await queryInterface.removeColumn('contacts', 'is_welcome_sent');
-  },
+  }
 };
